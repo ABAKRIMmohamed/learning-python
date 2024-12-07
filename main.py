beee34a066a666c4bcba774ec9b7cfe5279c8ef3
unknown <contact.abakrim@gmail.com>
2024-12-07 15:22:36 +0100

    learing a little about exceptions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import sys
 
 print(sys.version_info[0])
 
@@ -9,6 +10,30 @@ print("est" not in my_str)
 
 age = 36
 
-text = "hello I am {age} years old"
+text = f"hello I am {age} years old"
 
 print(text)
+
+
+# Seeing whether a value is in a range
+1 < 2 and 2 < 3  # => True
+2 < 3 and 3 < 2  # => False
+# Chaining makes this look nicer
+1 < 2 < 3  # => True
+2 < 3 < 2  # => False
+
+
+
+try:
+    f = open('myfile.txt')
+    s = f.readline()
+    i = int(s.strip())
+except OSError as err:
+    print("OS error:", err)
+except ValueError:
+    print("Could not convert data to an integer.")
+except Exception as err:
+    print(f"Unexpected {err=}, {type(err)=}")
+    raise
+
+raise NameError('HiThere')
